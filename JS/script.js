aa1254349179bae2f59dab6ca990f4f834416ca5
gulamansari57181 <gulamansari57181@gmail.com>
2022-02-08 22:03:15 +0530

    Fees page added

--- JS/script.js
+++ JS/script.js
@@ -1,59 +1,58 @@
 //   all ------------------
 function initParadoxWay() {
-  "use strict";
+    "use strict";
 
-  if ($(".testimonials-carousel").length > 0) {
-    var j2 = new Swiper(".testimonials-carousel .swiper-container", {
-      preloadImages: false,
-      slidesPerView: 1,
-      spaceBetween: 20,
-      loop: true,
-      grabCursor: true,
-      mousewheel: false,
-      centeredSlides: true,
-      pagination: {
-        el: ".tc-pagination",
-        clickable: true,
-        dynamicBullets: true,
-      },
-      navigation: {
-        nextEl: ".listing-carousel-button-next",
-        prevEl: ".listing-carousel-button-prev",
-      },
-      breakpoints: {
-        1024: {
-          slidesPerView: 3,
-        },
-      },
-    });
-  }
+    if ($(".testimonials-carousel").length > 0) {
+        var j2 = new Swiper(".testimonials-carousel .swiper-container", {
+            preloadImages: false,
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+            grabCursor: true,
+            mousewheel: false,
+            centeredSlides: true,
+            pagination: {
+                el: ".tc-pagination",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".listing-carousel-button-next",
+                prevEl: ".listing-carousel-button-prev",
+            },
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+    }
 
-  // bubbles -----------------
-  var size = randomValue(sArray);
-  setInterval(function () {
-    $(".bubbles").append(
-      '<div class="individual-bubble" style="left: ' +
-        randomValue(bArray) +
-        "px; width: " +
-        size +
-        "px; height:" +
-        size +
-        'px;"></div>'
-    );
-    $(".individual-bubble").animate(
-      {
-        bottom: "100%",
-        opacity: "-=0.7",
-      },
-      4000,
-      function () {
-        $(this).remove();
-      }
-    );
-  }, 350);
+    // bubbles -----------------
+    var size = randomValue(sArray);
+    setInterval(function() {
+        $(".bubbles").append(
+            '<div class="individual-bubble" style="left: ' +
+            randomValue(bArray) +
+            "px; width: " +
+            size +
+            "px; height:" +
+            size +
+            'px;"></div>'
+        );
+        $(".individual-bubble").animate({
+                bottom: "100%",
+                opacity: "-=0.7",
+            },
+            4000,
+            function() {
+                $(this).remove();
+            }
+        );
+    }, 350);
 }
 
 //   Init All ------------------
-$(document).ready(function () {
-  initParadoxWay();
+$(document).ready(function() {
+    initParadoxWay();
 });
